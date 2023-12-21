38b51cb4919a934ec7f911607c31e147729e5d4d
carregando <joojnow@gmail.com>
2023-12-21 01:20:50 -0400

    Criado input

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 import BackGroundImageWithChildren from '../src/components/layout/backgroundImageWithChildren/BackgroundImageWithChildren.jsx'
 import Logo from '../src/components/logo/Logo.jsx'
+import Input from '../src/components/form/input/Input.jsx'
 
 const StyledDiv = styled.div`
   background-color: ${props => props.theme.colors.white};
@@ -17,12 +18,18 @@ const StyledDiv = styled.div`
   }
 `
 
+const SpaceBetweenStyledDiv = styled.div`
+  margin-bottom: 70px;
+`
+
 export default function HomePage () {
   return (
     <BackGroundImageWithChildren>
       <StyledDiv>
-        <Logo showImage textType="vertical"/>
-        <input/>
+        <SpaceBetweenStyledDiv>
+          <Logo showImage textType="vertical"/>
+        </SpaceBetweenStyledDiv>
+        <Input placeholder = "Digite o nome da cidade" />
       </StyledDiv>
     </BackGroundImageWithChildren>
   )
